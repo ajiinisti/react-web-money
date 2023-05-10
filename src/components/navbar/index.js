cdf22b1d1bcaa7cf01cf5bf8b674c1e808888254
Aji Inisti Udma Wijaya <ajiinisti@gmail.com>
2023-05-10 12:20:57 +0700

    [ADD]Navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component{
+    render(){
+        return(
+            <header id="header" className="fixed-top bg-navbar-fixed">
+                <div className="container d-flex align-items-center">
+
+                <h1 className="logo me-auto"><a href="index.html">Belajar Saham Bersama</a></h1>
+
+                <nav id="navbar" className="navbar">
+                    <ul>
+                    <li><a className="nav-link scrollto" href="#hero">Beranda</a></li>
+                    <li><a className="nav-link scrollto" href="#about">Tentang Kami</a></li>
+                    <li><a className="nav-link scrollto" href="#services">Artikel</a></li>
+                    </ul>
+                    <i className="bi bi-list mobile-nav-toggle"></i>
+                </nav>
+
+                </div>
+            </header>
+        )
+    }
+}
