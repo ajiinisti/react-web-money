12a617c8638b9efb10f1221aa575afd162f9e531
Aji Inisti Udma Wijaya <ajiinisti@gmail.com>
2023-05-10 13:45:29 +0700

    [IMPROVE]Layout on hero section

--- src/pages/home/hero.jsx
+++ src/pages/home/hero.jsx
@@ -7,12 +7,12 @@ export default class Hero extends Component{
             <section id="hero" className="d-flex align-items-center">
                 <div className="container">
                 <div className="row">
+                    <div className="col-lg-6 order-1 order-rg-2 hero-img">
+                        <img src={StokPng} className="img-fluid animated" alt=""/>
+                    </div>
                     <div className="col-lg-6 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1" data-aos="fade-up" data-aos-delay="200">
                     <h1>Pusat Berita Saham Dan Cryptocurrency Terpercaya</h1>
                     <h2>Tempat untuk kamu yang ingin berdiskusi, analisa dan investasi saham dalam satu tempat.</h2>
-                    </div>
-                    <div className="col-lg-6 order-1 order-lg-2 hero-img">
-                        <img src={StokPng} className="img-fluid animated" alt=""/>
                     </div>
                 </div>
                 </div>
