ccf5e6ea68c1aa5689cb54b08638cac192fedbf2
Aji Inisti Udma Wijaya <ajiinisti@gmail.com>
2023-05-10 12:19:47 +0700

    [ADD]Image and Css

---
+++ src/asset/img/clients/index.js
@@ -0,0 +1,17 @@
+import Artikel1 from "./artikel1.png"
+import Artikel2 from "./artikel2.png"
+import Artikel3 from "./artikel3.png"
+import Artikel4 from "./artikel4.png"
+import Carousel1 from "./carousel1.jpg"
+import StokPng from "./stock.png"
+import FamPhoto from "./family-photo.jpeg"
+
+export{
+    Artikel1,
+    Artikel2,
+    Artikel3,
+    Artikel4,
+    Carousel1,
+    StokPng,
+    FamPhoto
+}
---
+++ src/asset/img/index.js
@@ -0,0 +1,11 @@
+import InstagramLogo from "./instagram.svg";
+import TwitterLogo from "./twitter.svg";
+import YoutubeLogo from "./yt.svg";
+import WaLogo from "./wa.svg";
+
+export{
+    InstagramLogo,
+    TwitterLogo,
+    YoutubeLogo,
+    WaLogo
+}
